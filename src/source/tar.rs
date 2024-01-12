c96384fdd38f0fd5ba991fa245ca66c42f792215
Benoît du Garreau <bdgdlm@outlook.com>
2024-01-12 16:07:13 +0100

    Read bytes slices from TAR directly if possible

--- src/source/tar.rs
+++ src/source/tar.rs
@@ -163,15 +163,11 @@ fn register_file(
 /// A [`Source`] to load assets from a tar archive.
 ///
 /// The archive can be backed by any reader that also implements [`io::Seek`]
-/// and [`Clone`].
-///
-/// **Warning**: This will clone the reader each time it is read, so you should
-/// ensure that is cheap to clone (eg *not* `Vec<u8>`).
+/// and [`Clone`] or by a byte slice. In the second case, reading files will
+/// not involve copying data.
 pub struct Tar<R = SyncFile> {
-    reader: R,
-    files: HashMap<FileDesc, (u64, u64)>,
-    dirs: HashMap<SharedString, Vec<OwnedEntry>>,
-    label: Option<String>,
+    inner: UntypedTar,
+    _typ: std::marker::PhantomData<R>,
 }
 
 impl Tar<SyncFile> {
@@ -190,36 +186,97 @@ impl Tar<SyncFile> {
 
 impl<T: AsRef<[u8]>> Tar<io::Cursor<T>> {
     /// Creates a `Tar` archive backed by a byte buffer in memory.
+    ///
+    /// Reading a file from this archive will not copy its content.
     #[inline]
     pub fn from_bytes(bytes: T) -> io::Result<Self> {
-        Self::from_reader(io::Cursor::new(bytes))
+        Self::new(io::Cursor::new(bytes), Bytes, None)
     }
 
     /// Creates a `Tar` archive backed by a byte buffer in memory.
+    ///
+    /// Reading a file from this archive will not copy its content.
+    ///
+    /// An additionnal label that will be used in errors can be added.
     #[inline]
     pub fn from_bytes_with_label(bytes: T, label: String) -> io::Result<Self> {
-        Self::from_reader_with_label(io::Cursor::new(bytes), label)
+        Self::new(io::Cursor::new(bytes), Bytes, Some(label))
     }
 }
 
 impl<R> Tar<R>
 where
-    R: io::Read + io::Seek,
+    R: io::Read + io::Seek + Clone,
 {
     /// Creates a `Tar` archive backed by a reader that supports seeking.
+    ///
+    /// **Warning**: This will clone the reader each time it is read, so you should
+    /// ensure that is cheap to clone.
     pub fn from_reader(reader: R) -> io::Result<Self> {
-        Self::create(reader, None)
+        Self::new(reader, Reader, None)
     }
 
     /// Creates a `Tar` archive backed by a reader that supports seeking.
     ///
     /// An additionnal label that will be used in errors can be added.
+    ///
+    /// **Warning**: This will clone the reader each time it is read, so you should
+    /// ensure that is cheap to clone.
     pub fn from_reader_with_label(reader: R, label: String) -> io::Result<Self> {
-        Self::create(reader, Some(label))
+        Self::new(reader, Reader, Some(label))
+    }
+}
+
+impl<R> Tar<R> {
+    fn new<F, T>(reader: R, f: F, label: Option<String>) -> io::Result<Self>
+    where
+        F: FnOnce(R) -> T,
+        T: TarReader,
+    {
+        Ok(Tar {
+            inner: UntypedTar::new(Box::new(f(reader)), label)?,
+            _typ: std::marker::PhantomData,
+        })
+    }
+}
+
+#[cfg_attr(docsrs, doc(cfg(feature = "tar")))]
+impl<R> super::Source for Tar<R> {
+    #[inline]
+    fn read(&self, id: &str, ext: &str) -> io::Result<super::FileContent> {
+        self.inner.read(id, ext)
     }
 
-    fn create(reader: R, label: Option<String>) -> io::Result<Self> {
-        let mut archive = tar::Archive::new(reader);
+    #[inline]
+    fn read_dir(&self, id: &str, f: &mut dyn FnMut(DirEntry)) -> io::Result<()> {
+        self.inner.read_dir(id, f)
+    }
+
+    #[inline]
+    fn exists(&self, entry: DirEntry) -> bool {
+        self.inner.exists(entry)
+    }
+}
+
+impl<R> fmt::Debug for Tar<R> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.inner.fmt(f)
+    }
+}
+
+struct UntypedTar {
+    // Unsafe container without any lifetime marker nor Send/Sync bounds.
+    // They are added by the `PhantomData` field in `Tar`.
+    reader: AllAutoTraits<Box<dyn TarReader>>,
+
+    files: HashMap<FileDesc, (u64, u64)>,
+    dirs: HashMap<SharedString, Vec<OwnedEntry>>,
+    label: Option<String>,
+}
+
+impl UntypedTar {
+    fn new(mut reader: Box<dyn TarReader + '_>, label: Option<String>) -> io::Result<Self> {
+        let mut archive = tar::Archive::new(reader.read_seeker());
         let mut id_builder = IdBuilder::default();
 
         let mut files = HashMap::new();
@@ -229,8 +286,10 @@ where
             register_file(file?, &mut files, &mut dirs, &mut id_builder)
         }
 
-        Ok(Tar {
-            reader: archive.into_inner(),
+        let reader = unsafe { std::mem::transmute(reader) };
+
+        Ok(UntypedTar {
+            reader: AllAutoTraits(reader),
             files,
             dirs,
             label,
@@ -239,25 +298,17 @@ where
 }
 
 #[cfg_attr(docsrs, doc(cfg(feature = "tar")))]
-impl<R> super::Source for Tar<R>
-where
-    R: io::Read + io::Seek + Clone,
-{
+impl super::Source for UntypedTar {
     fn read(&self, id: &str, ext: &str) -> io::Result<super::FileContent> {
         let &(start, size) = self
             .files
             .get(&(id, ext) as &dyn FileKey)
             .ok_or_else(|| error::find_file(id, &self.label))?;
 
-        let mut reader = self.reader.clone();
-
-        let mut buf = vec![0; size as usize];
-        reader
-            .seek(io::SeekFrom::Start(start))
-            .and_then(|_| reader.read_exact(&mut buf))
-            .map_err(|err| error::read_file(err, id, &self.label))?;
-
-        Ok(super::FileContent::Buffer(buf))
+        self.reader
+            .0
+            .read_file(start, size as usize)
+            .map_err(|err| error::read_file(err, id, &self.label))
     }
 
     fn read_dir(&self, id: &str, f: &mut dyn FnMut(DirEntry)) -> io::Result<()> {
@@ -277,12 +328,62 @@ where
     }
 }
 
-impl<R> fmt::Debug for Tar<R> {
+impl fmt::Debug for UntypedTar {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("Tar")
             .field("label", &self.label)
             .field("dirs", &self.dirs)
             .finish()
+    }
+}
+
+struct AllAutoTraits<T>(T);
+
+unsafe impl<T> Send for AllAutoTraits<T> {}
+unsafe impl<T> Sync for AllAutoTraits<T> {}
+impl<T> std::panic::UnwindSafe for AllAutoTraits<T> {}
+impl<T> std::panic::RefUnwindSafe for AllAutoTraits<T> {}
+
+trait ReadSeek: io::Read + io::Seek {}
+impl<R: io::Read + io::Seek> ReadSeek for R {}
+
+trait TarReader {
+    fn read_file(&self, start: u64, size: usize) -> io::Result<super::FileContent<'_>>;
+
+    fn read_seeker(&mut self) -> &mut dyn ReadSeek;
+}
+
+struct Reader<R>(R);
+struct Bytes<T>(io::Cursor<T>);
+
+impl<R: io::Read + io::Seek + Clone> TarReader for Reader<R> {
+    fn read_file(&self, start: u64, size: usize) -> io::Result<super::FileContent<'_>> {
+        let mut reader = self.0.clone();
+
+        let mut buf = vec![0; size];
+        reader.seek(io::SeekFrom::Start(start))?;
+        reader.read_exact(&mut buf)?;
+
+        Ok(super::FileContent::Buffer(buf))
+    }
+
+    fn read_seeker(&mut self) -> &mut dyn ReadSeek {
+        &mut self.0
+    }
+}
+
+impl<T: AsRef<[u8]>> TarReader for Bytes<T> {
+    fn read_file(&self, start: u64, size: usize) -> io::Result<super::FileContent<'_>> {
+        let start = start as usize;
+        let tar = self.0.get_ref().as_ref();
+        let file = tar
+            .get(start..start + size)
+            .ok_or(io::ErrorKind::InvalidData)?;
+        Ok(super::FileContent::Slice(file))
+    }
+
+    fn read_seeker(&mut self) -> &mut dyn ReadSeek {
+        &mut self.0
     }
 }
 
